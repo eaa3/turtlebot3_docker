5145254203e503550887f0e2a5e215649fad553b
Ermano Arruda <ermano.arruda@gmail.com>
2019-08-06 15:41:25 +0100

    uses first running container if parameter is not passed

--- exec_container.sh
+++ exec_container.sh
@@ -2,22 +2,23 @@
 
 ROOT_DIR="$(cd $( dirname ${BASH_SOURCE[0]} ) && pwd)"
 
-if [ $# -ne 1 ]; then
-	echo "usage: ./exec_container.sh <container_id>"
-    echo "example: ./docker_build.sh fa11998579c"
-    echo "to get current container id, run ./get_containerId.sh script"
-  exit 1
-fi
-
-# CONTAINER_ID=$1
-
-# if [ -z "$CONTAINER_ID" ]
-# then
-#       echo "usage: ./exec_container.sh <container_id>"
-#       echo "example: ./docker_build.sh fa11998579c"
-#       echo "to get current container id, run ./get_containerId.sh script"
-#       exit 1
+# if [ $# -ne 1 ]; then
+# 	echo "usage: ./exec_container.sh <container_id>"
+#     echo "example: ./docker_build.sh fa11998579c"
+#     echo "to get current container id, run ./get_containerId.sh script"
+#   exit 1
 # fi
+
+CONTAINER_ID=$1
+
+if [ -z "$CONTAINER_ID" ]
+then
+      echo "usage: ./exec_container.sh <container_id>"
+      echo "example: ./docker_build.sh fa11998579c"
+      echo "to get current container id, run ./get_containerId.sh script"
+      CONTAINER_ID=$(docker ps -l -q)
+      # exit 1
+fi
 
 
 shopt -s expand_aliases
@@ -25,6 +26,6 @@ source $HOME/.bashrc
 source ${ROOT_DIR}/aml_aliases.sh
 
 
-echo 'Entering container:' $1
-xdocker exec -it $1 bash
+echo 'Entering container:' $CONTAINER_ID
+xdocker exec -it $CONTAINER_ID bash
 # -c "source /opt/ros/kinetic/setup.bash && /bin/bash"
